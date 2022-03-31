040bf531f7bc9f668630d1a9df51eccbc55a62c3
Marcos Guedes <84164595+MarcosGuedess@users.noreply.github.com>
2022-03-31 09:24:34 -0300

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     session_start();
     if($_POST['username'] == 'Vitor' and $_POST['password'] == 'senha'){
         $_SESSION['loggedin'] = TRUE;
-        $_SESSION["username"] = 'Vitor Henrique';
+        $_SESSION["username"] = 'Marcos Guedes';
          header("location: welcome.php");
     } else {
         $_SESSION['loggedin'] = FALSE;
@@ -31,7 +31,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
             <div class="form-group">
                 <label>Username</label>
-                <input type="text" name="username" class="form-control" value="Vitor">
+                <input type="text" name="username" class="form-control" value="Marcos">
                 <span class="help-block"></span>
             </div>    
             <div class="form-group">
